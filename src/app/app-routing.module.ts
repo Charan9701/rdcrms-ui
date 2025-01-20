8c7d3382f0eb800e57adca728b68a2ebc8a31731
Charan9701 <charanmungara2005@gmail.com>
2025-01-20 19:59:03 +0530

    change password ui added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeaderComponent } from './header/header.component';
 import { ReportsComponent } from './reports/reports.component';
 import { UserRegisterationComponent } from './registration/user-registeration/user-registeration.component';
 import { AllUsersComponent } from './all-users/all-users.component';
@@ -10,12 +9,14 @@ import { CasteComponent } from './caste/caste.component';
 import { IncomeComponent } from './income/income.component';
 import { BirthComponent } from './birth/birth.component';
 import { DeathComponent } from './death/death.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 
 const routes: Routes = [
  { path: "", component: HomeComponent},
  { path: "home", component: HomeComponent},
  { path: "register", component: UserRegisterationComponent},
+ { path: "change-password", component: ChangePasswordComponent},
  { path: "dashboard", component: DashboardComponent, 
  children: [
   { path: "", component: ReportsComponent},
